469ba3febab09d4acc404ffabddb7e6dd74fa952
Léo Nord462 <francois.perrin@tuta.io>
2023-10-24 17:27:58 +0200

    nice indent

--- src/game_engine.py
+++ src/game_engine.py
@@ -44,65 +44,64 @@ class GameEngine:
     def current_day(self) -> int:
         return self._day
 
-    @property
-    def summary(self) -> str:
-        return (
-            f"World : {self._world}, Colony : {self.colony}, "
-            f"Number of players alive on the isle : {len(self.colony.alive_players)}"
-        )
-
     def update(self) -> Generator[str, None, None]:
         """This updates the game and make the _actions of a complete day, from dawn to dawn"""
+
+        # Step zero, world update
         self._day += 1
-
-        yield f"--- THE DAY #{self._day} STARTS"
-
-        # Step zero
         self._world.update()
-        yield f"- Weather report : {self._world.weather.name}"
-        yield f"- World resources : {self._world}"
+
+        yield f"--- DAWN OF DAY #{self._day} ({self._world.weather.name})"
+        yield f"/ ---"
+        yield f"| World : {self._world}"
+        yield f"| Colony : {self.colony}"
+        yield f"| {len(self.colony.alive_players)} players lefts"
+        yield f"\\ ---"
 
         # First step : daily _actions
-        yield "--- EVERY ONE WORK"
-
         for player in self.colony.alive_players:
             for log in player.make_random_daily_action():
-                yield log
+                yield f"  > {log}"
 
-        yield f"--- NIGHT FALLS, COLONY RESOURCES : {self.colony}"
+        yield ""
+        yield f"--- SUN GETS DOWN - {self.colony}"
 
         # Second step : Some must die
         if not self.colony.enough_resources:
-            yield "NOT ENOUGH RESOURCES"
             limiting_factor = self.colony.limiting_factor
             amount_of_players_to_die = len(self.colony.alive_players) - limiting_factor
-            yield f"{amount_of_players_to_die} players must die."
+            yield f"NOT ENOUGH RESOURCES : {amount_of_players_to_die} players must die."
 
             for i in range(0, amount_of_players_to_die):
                 player_to_die = self.colony.get_random_alive_player()
                 player_to_die.die(self.current_day)
-                yield f"{player_to_die} died on day #{player_to_die.day_of_death}."
+                yield f"  X {player_to_die} died on day #{player_to_die.day_of_death}."
 
         if not self.game_over:
             # Third step : Diner
-            yield f"--- EVERY ONE EATS DINER"
-            for _ in self.colony.dine():
-                ...
+            yield f"EVERY ONE DINE"
+            for player in self.colony.dine():
+                yield f"  - {player} dine"
 
             # Fourth step : Verify if there's enough resources to leave
             if self.colony.able_to_leave:
+                yield f"SOME LEAVE THE ISLE !"
                 count = 0
                 for player in self.colony.leave_isle():
-                    yield f"{player} took the raft"
+                    yield f"  >> {player} took the raft"
                     count += 1
                 yield f"--------- {count} PLAYERS ESCAPED ---------"
 
             # Fifth step : Close your eyes and sleep my darling
             else:
-                yield "--------- THE NEXT DAY ------------"
+                yield "--------- NIGHT FALLS ------------"
 
         else:
             yield "--------- GAME OVER ------------"
 
         # Day summary
-        yield self.summary
+        yield f"/ ---"
+        yield f"| World : {self._world}"
+        yield f"| Colony : {self.colony}"
+        yield f"| {len(self.colony.alive_players)} players lefts"
+        yield f"\\ ---"
